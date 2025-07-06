0505cd6de5425c0dc06929b69466d0b7b53713ef
Jiming Chen <jc3579@cornell.edu>
2025-07-06 01:48:45 -0500

    Separate setup and inference scripts

--- inference.sh
+++ inference.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-set -e  # Exit on error
-
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
-
-# Install Python package
-pip install -e .
-
-# Download model
-python download_wan2.1.py
-
 # Run inference
 python inference_recammaster.py --cam_type 1
